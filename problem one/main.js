74896364ea6438d2dec7ffc3995777f390168bf7
rirakib <rirrakib10@gmail.com>
2022-04-30 14:36:43 +0600

    first commit

---
+++ problem one/main.js
@@ -0,0 +1,20 @@
+//problem ---> two integer sum and avarage
+
+//Algorithem 
+
+// 1. input value X And Y 
+// 2. set Sum = X + Y 
+// 3. set Avg = Sum/2
+// 4. Display Sum and Avg
+// 5. Exit
+
+
+var x = 6;
+var y = 4;
+var sum = x + y;
+var avg = sum/2;
+console.log(`sum = ${sum} and avg = ${avg}`);
+
+
+
+
